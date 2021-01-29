b5c935e1a5cf34909c41005e0948d10223abed55
YaniraMurriel <yaniscase@hotmail.com>
2021-01-29 17:01:08 -0500

    reto002

---
+++ src/test/java/com/pruple/reto002/reto002/Reto002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruple.reto002.reto002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
